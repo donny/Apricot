ff8dbce9722a39799ab0fe65203e8df37b035d17
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-10-02 07:44:44 +1100

    Initial files from Xcode

---
+++ Apricot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Apricot
+//
+//  Created by Donny Kurniawan on 2/10/17.
+//  Copyright © 2017 Donny Kurniawan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
