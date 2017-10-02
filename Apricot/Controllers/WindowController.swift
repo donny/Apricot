114326a17d73efa6eb28dcfb72506305460c6a26
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-10-03 09:15:42 +1100

    Add a basic toolbar

---
+++ Apricot/Controllers/WindowController.swift
@@ -0,0 +1,17 @@
+//
+//  WindowController.swift
+//  Apricot
+//
+//  Created by Donny Kurniawan on 3/10/17.
+//  Copyright © 2017 Donny Kurniawan. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+  override func windowDidLoad() {
+    super.windowDidLoad()
+    window?.titleVisibility = .hidden
+    window?.isExcludedFromWindowsMenu = true
+  }
+}
