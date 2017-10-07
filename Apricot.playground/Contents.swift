03363a0690567092d8d8bf84bf1539fd1ed63471
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-10-08 08:22:00 +1100

    Add a commit trace function

--- Apricot.playground/Contents.swift
+++ Apricot.playground/Contents.swift
@@ -6,14 +6,30 @@ import Result
 
 var str = "Hello, playground"
 
+func trace(repository: Repository, commit: Commit) -> Void {
+  print("Commit: \(commit.message)")
+  commit.parents.forEach { (parent) in
+    let parentCommit = repository.commit(parent.oid)
+    if let parentCommit = parentCommit.value {
+      trace(repository: repository, commit: parentCommit)
+    }
+  }
+}
+
+
+
 let url = URL(fileURLWithPath: "/Users/donny.kurniawan/unix/projects/github/Apricot")
 let repo = Repository.at(url)
 if let repo = repo.value {
   let latestCommit: Result<Commit, NSError> = repo
     .HEAD()
-    .flatMap { repo.commit($0.oid) }
+    .flatMap { ref in
+      return repo.commit(ref.oid)
+    }
   if let commit = latestCommit.value {
-    print("Latest Commit: \(commit.message) by \(commit.author.name)")
+    // print("Latest Commit: \(commit.message) by \(commit.author.name)")
+
+    trace(repository: repo, commit: commit)
   } else {
     print("Could not get commit: \(latestCommit.error!)")
   }
