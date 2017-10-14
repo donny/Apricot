16f1202d71e65a391816cdad5f562f899f7f9b5a
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-10-15 07:47:31 +1100

    WIP - 2 nodes and 1 line

--- Apricot/Scenes/MainScene.swift
+++ Apricot/Scenes/MainScene.swift
@@ -17,5 +17,40 @@ class MainScene: SKScene {
       label.alpha = 0.0
       label.run(SKAction.fadeIn(withDuration: 2.0))
     }
+
+
+
+    let w = (self.size.width + self.size.height) * 0.05
+    let spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+
+    spinnyNode.lineWidth = 2.5
+//    spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+//    spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 2.0),
+//                                      SKAction.fadeOut(withDuration: 2.0),
+//                                      SKAction.removeFromParent()]))
+    spinnyNode.position = CGPoint(x: -100, y: -100)
+    spinnyNode.strokeColor = SKColor.blue
+
+    addChild(spinnyNode)
+
+
+    let spinnyNode2 = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+    spinnyNode2.lineWidth = 2.5
+    spinnyNode2.position = CGPoint(x: -100, y: 100)
+    spinnyNode2.strokeColor = SKColor.blue
+    addChild(spinnyNode2)
+
+
+    var path = CGMutablePath()
+    path.move(to: CGPoint(x: 100, y: 100))
+    path.addLine(to: CGPoint(x: 500, y: 500))
+
+    let shape = SKShapeNode()
+    shape.path = path
+    shape.strokeColor = SKColor.red
+    shape.lineWidth = 2
+    addChild(shape)
+
+
   }
 }
