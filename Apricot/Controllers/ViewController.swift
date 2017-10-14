f5b7226f8ef50d7bcc1d495b1ee194f3a629bf19
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-10-14 16:55:31 +1100

    Refactor

--- Apricot/Controllers/ViewController.swift
+++ Apricot/Controllers/ViewController.swift
@@ -12,69 +12,54 @@ import SwiftGit2
 import Result
 
 class ViewController: NSViewController {
-    @IBOutlet var sceneKitView: SKView!
-    var repository: Repository? {
-        didSet {
-            refreshView()
-        }
+  @IBOutlet var sceneKitView: SKView!
+  var repository: Repository? {
+    didSet {
+      refreshView()
     }
-    var branchName: String? {
-        didSet {
-            refreshView()
-        }
+  }
+  var branchName: String? {
+    didSet {
+      refreshView()
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if let view = self.sceneKitView {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    loadScene(name: "InitialScene")
+  }
+
+  private func loadScene(name: String) {
+    if let view = self.sceneKitView {
+      if let scene = SKScene(fileNamed: name) {
+        scene.scaleMode = .aspectFill
+        view.presentScene(scene)
+      }
+      view.ignoresSiblingOrder = true
+      view.showsFPS = true
+      view.showsNodeCount = true
     }
-    
-    private func loadErrorView() {
-        if let view = self.sceneKitView {
-            if let scene = SKScene(fileNamed: "ErrorScene") {
-                scene.scaleMode = .aspectFill
-                view.presentScene(scene)
-            }
-            view.ignoresSiblingOrder = true
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+  }
+
+  private func refreshView() {
+    if repository == nil || branchName == nil {
+      loadScene(name: "ErrorScene")
     }
-    
-    private func refreshView() {
-        if repository == nil || branchName == nil {
-            loadErrorView()
-        }
+  }
+
+  func showRepository(url: URL) {
+    let repo = Repository.at(url)
+    if let repo = repo.value {
+      let latestCommit: Result<Commit, NSError> = repo
+        .HEAD()
+        .flatMap { repo.commit($0.oid) }
+      if let commit = latestCommit.value {
+        print("Latest Commit: \(commit.message) by \(commit.author.name)")
+      } else {
+        print("Could not get commit: \(latestCommit.error!)")
+      }
+    } else {
+      print("Could not open repository: \(repo.error!)")
     }
-    
-    func showRepository(url: URL) {
-        let repo = Repository.at(url)
-        if let repo = repo.value {
-            let latestCommit: Result<Commit, NSError> = repo
-                .HEAD()
-                .flatMap { repo.commit($0.oid) }
-            if let commit = latestCommit.value {
-                print("Latest Commit: \(commit.message) by \(commit.author.name)")
-            } else {
-                print("Could not get commit: \(latestCommit.error!)")
-            }
-        } else {
-            print("Could not open repository: \(repo.error!)")
-        }
-    }
+  }
 }
--- Apricot/Controllers/WindowController.swift
+++ Apricot/Controllers/WindowController.swift
@@ -11,55 +11,56 @@ import SwiftGit2
 import Result
 
 class WindowController: NSWindowController {
-    @IBOutlet var popUpButton: NSPopUpButton!
+  @IBOutlet var popUpButton: NSPopUpButton!
+  
+  override func windowDidLoad() {
+    super.windowDidLoad()
+    popUpButton.removeAllItems()
+  }
+  
+  @IBAction func performOpenRepositoryAction(_ sender: NSButton) {
+    openRepository()
+  }
+  
+  @IBAction func performBranchSelectedAction(_ sender: NSPopUpButton) {
+    guard let controller = self.contentViewController as? ViewController else { return }
+    controller.branchName = sender.itemTitle(at: sender.indexOfSelectedItem)
+  }
+  
+  func openRepository() {
+    guard let window = self.window,
+      let controller = self.contentViewController as? ViewController
+      else { return }
     
-    override func windowDidLoad() {
-        super.windowDidLoad()
-    }
+    let openPanel = NSOpenPanel()
+    openPanel.allowsMultipleSelection = false
+    openPanel.canChooseDirectories = true
+    openPanel.canChooseFiles = false
     
-    @IBAction func performOpenRepositoryAction(_ sender: NSButton) {
-        openRepository()
-    }
-    
-    @IBAction func performBranchSelectedAction(_ sender: NSPopUpButton) {
-        guard let controller = self.contentViewController as? ViewController else { return }
-        controller.branchName = sender.itemTitle(at: sender.indexOfSelectedItem)
-    }
-    
-    func openRepository() {
-        guard let window = self.window,
-            let controller = self.contentViewController as? ViewController
-            else { return }
-        
-        let openPanel = NSOpenPanel()
-        openPanel.allowsMultipleSelection = false
-        openPanel.canChooseDirectories = true
-        openPanel.canChooseFiles = false
-        
-        openPanel.beginSheetModal(for: window, completionHandler: { response in
-            switch response {
-            case .OK:
-                if let url = openPanel.urls.first {
-                    if let repository = Repository.at(url).value,
-                        let branches = repository.localBranches().value,
-                        branches.count > 0 {
-                        controller.repository = repository
-                        controller.branchName = branches.first?.name
-                        
-                        self.popUpButton.removeAllItems()
-                        branches.forEach {
-                            self.popUpButton.addItem(withTitle: $0.name)
-                        }
-                        self.performBranchSelectedAction(self.popUpButton)
-                    } else {
-                        print("ERROR: could not open repository or no branches")
-                        controller.repository = nil
-                        controller.branchName = nil
-                    }
-                }
-            default:
-                break
+    openPanel.beginSheetModal(for: window, completionHandler: { response in
+      switch response {
+      case .OK:
+        if let url = openPanel.urls.first {
+          if let repository = Repository.at(url).value,
+            let branches = repository.localBranches().value,
+            branches.count > 0 {
+            controller.repository = repository
+            controller.branchName = branches.first?.name
+            
+            self.popUpButton.removeAllItems()
+            branches.forEach {
+              self.popUpButton.addItem(withTitle: $0.name)
             }
-        })
-    }
+            self.performBranchSelectedAction(self.popUpButton)
+          } else {
+            print("ERROR: could not open repository or no branches")
+            controller.repository = nil
+            controller.branchName = nil
+          }
+        }
+      default:
+        break
+      }
+    })
+  }
 }
